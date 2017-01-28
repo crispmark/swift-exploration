d09147559a59d89fcbd77fd99e21260824c2008d
Mark Crisp <macr1324@gmail.com>
2017-01-28 15:58:59 -0500

    Create primefactor-calculation.swift

---
+++ numbers/primefactor-calculation.swift
@@ -0,0 +1,40 @@
+// Calculates the prime factors of a provided integer
+
+import Foundation
+
+class PrimeFactor {
+    func calculate(n: Int) -> [Int] {
+        var primeFactors = [Int]()
+        let halfN: Int = n/2
+        for i in 1...halfN {
+            if (((n%i) == 0) && prime(n: i)) {
+                primeFactors.append(i)
+            }
+        }
+        if (prime(n: n)) {
+            primeFactors.append(n)
+        }
+        return primeFactors
+    }
+}
+
+func prime(n: Int) -> Bool {
+    return factors(n: n).count <= 2
+}
+
+func factors(n: Int) -> [Int] {
+    var factors = [Int]()
+    factors.append(1)
+    let halfN: Int = n/2
+    if (halfN >= 2) {
+        for i in 2...halfN {
+            if ((n%i) == 0) {
+                factors.append(i)
+            }
+        }
+    }
+    if (n != 1) {
+        factors.append(n)
+    }
+    return factors
+}
